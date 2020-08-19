016e858c7d3c643105ffe6a36a78a5032d2d96e1
Ian Foo <ianshalom92@gmail.com>
2020-08-19 16:40:22 +0800

    Added bare bones structure and tested axios with Alpha Vantage API key

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = process.env.REACT_APP_ALPHA_API_KEY;
+
+class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    axios
+      .get(
+        // `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=NFLX&apikey=${API_KEY}`
+        `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=INTC&apikey=${API_KEY}`
+      )
+      .then((res) => {
+        console.log(res.data);
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>SHALOM</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
