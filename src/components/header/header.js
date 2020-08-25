f5cf00a897570f3a4015c0c2a12db25199f93d55
Ian Foo <ianshalom92@gmail.com>
2020-08-25 10:18:49 +0800

    Made page mobile compatible

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,6 +4,7 @@ import "./Header.css";
 function Header() {
   return (
     <div className="header">
+      <span className="bambu">Bambu FE Test </span>
       <span className="title">OHLC Chart </span>
       <span className="date">2019</span>
     </div>
